6215a27aea5a6ed18ebe1e1c8ad6766ea63075b0
Julien <julien@ripouteau.com>
2023-07-12 14:48:48 +0200

    refactor: move to japa 3

--- bin/test.ts
+++ bin/test.ts
@@ -1,49 +1,20 @@
 import { assert } from '@japa/assert'
-import { specReporter } from '@japa/spec-reporter'
 import { fileSystem } from '@japa/file-system'
 import { expectTypeOf } from '@japa/expect-type'
-import { runFailedTests } from '@japa/run-failed-tests'
-import { processCliArgs, configure, run } from '@japa/runner'
-import { pathToFileURL } from 'node:url'
+import { processCLIArgs, configure, run } from '@japa/runner'
 import { BASE_URL } from '../tests_helpers/index.js'
 
-/*
-|--------------------------------------------------------------------------
-| Configure tests
-|--------------------------------------------------------------------------
-|
-| The configure method accepts the configuration to configure the Japa
-| tests runner.
-|
-| The first method call "processCliArgs" process the command line arguments
-| and turns them into a config object. Using this method is not mandatory.
-|
-| Please consult japa.dev/runner-config for the config docs.
-*/
+processCLIArgs(process.argv.slice(2))
 configure({
-  ...processCliArgs(process.argv.slice(2)),
-  ...{
-    files: ['tests/**/*.spec.ts'],
-    plugins: [
-      assert(),
-      fileSystem({
-        autoClean: true,
-        basePath: BASE_URL,
-      }),
-      expectTypeOf(),
-      runFailedTests(),
-    ],
-    reporters: [specReporter({ stackLinesCount: 2 })],
-    importer: (filePath) => import(pathToFileURL(filePath).href),
-  },
+  files: ['tests/**/*.spec.ts'],
+  plugins: [
+    assert(),
+    fileSystem({
+      autoClean: true,
+      basePath: BASE_URL,
+    }),
+    expectTypeOf(),
+  ],
 })
 
-/*
-|--------------------------------------------------------------------------
-| Run tests
-|--------------------------------------------------------------------------
-|
-| The following "run" method is required to execute all the tests.
-|
-*/
 run()
