1f59e13399f884e88e0665445e9064a8e3fa71e9
Julien <julien@ripouteau.com>
2023-07-12 15:45:46 +0200

    test: add test for configure task

---
+++ tests/configure.spec.ts
@@ -0,0 +1,45 @@
+/*
+ * @adonisjs/view
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { test } from '@japa/runner'
+import { IgnitorFactory } from '@adonisjs/core/factories'
+import Configure from '@adonisjs/core/commands/configure'
+import { BASE_URL } from '../tests_helpers/index.js'
+
+test.group('Configure', () => {
+  test('create config file and register provider', async ({ assert, fs }) => {
+    const ignitor = new IgnitorFactory()
+      .withCoreProviders()
+      .withCoreConfig()
+      .create(BASE_URL, {
+        importer: (filePath) => {
+          if (filePath.startsWith('./') || filePath.startsWith('../')) {
+            return import(new URL(filePath, BASE_URL).href)
+          }
+
+          return import(filePath)
+        },
+      })
+
+    await fs.create('.env', '')
+
+    const app = ignitor.createApp('web')
+    await app.init()
+    await app.boot()
+
+    const ace = await app.container.make('ace')
+    const command = await ace.create(Configure, ['../../index.js'])
+    command.ui.switchMode('raw')
+    await command.exec()
+
+    await assert.fileExists('vite.config.js')
+    await assert.fileContains('.adonisrc.json', '@adonisjs/vite/providers/vite_provider')
+    await assert.fileContains('vite.config.js', 'import Adonis from "@adonisjs/vite/plugin"')
+  })
+})
