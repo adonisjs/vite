b5a6ad7b03e8c39456828310f7728c2862db1e4d
Harminder Virk <virk.officials@gmail.com>
2025-09-27 19:13:13 +0530

    chore: update dependencies

--- src/hooks/build_hook.ts
+++ src/hooks/build_hook.ts
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-import { hooks } from '@adonisjs/assembler'
+import { hooks } from '@adonisjs/core/app'
 import { createBuilder } from 'vite'
 
 /**
